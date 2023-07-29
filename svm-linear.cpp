91ed688bf1bd9df91158b4c8eb0b0381bd4bede1
Caesar <caesar.etos@gmail.com>
2023-07-29 18:43:16 -0400

    First commit

---
+++ svm-linear.cpp
@@ -0,0 +1,283 @@
+//==================================================================================
+// BSD 2-Clause License
+//
+// Copyright (c) 2014-2022, NJIT, Duality Technologies Inc. and other contributors
+//
+// All rights reserved.
+//
+// Author TPOC: contact@openfhe.org
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//==================================================================================
+
+/*
+  Simple examples for CKKS
+ */
+
+#define PROFILE
+
+#include "openfhe.h"
+
+using namespace lbcrypto;
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void print_double_vector_comma_separated(const vector<double>& data, const string& label) {
+    cout << label << ": [";
+    for (size_t i = 0; i < data.size() - 1; i++) {
+        cout << setprecision(15) << data[i] << ", ";
+    }
+    cout << setprecision(15) << data[data.size() - 1] << "]" << endl;
+}
+
+vector<double> read_double_data_from_file(const string& filename) {
+    vector<double> data;
+    ifstream in(filename);
+    if (!in) {
+        cerr << "Could not open file " << filename << endl;
+        return data;
+    }
+
+    double number;
+    while (in >> number) {
+        data.push_back(number);
+    }
+
+    // Close the file
+    in.close();
+
+    return data;
+}
+
+void resize_double_vector(vector<double>& data, uint32_t new_size) {
+    // If the new size is smaller than the current size,
+    // we need to shorten the vector.
+    if (new_size < data.size()) {
+        data.resize(new_size);
+    } else {
+        // If the new size is larger than the current size,
+        // we need to add new entries and zero them out.
+        for (size_t i = data.size(); i < new_size; i++) {
+            data.push_back(0.0);
+        }
+    }
+}
+
+
+int main() {
+
+    cout << "SVM linear started ... !\n\n";
+
+    uint32_t n = 4; // SVM vectors dimensions (# of predictors)
+
+    // read the data
+    vector<double> weights = read_double_data_from_file("../data-linear-model/weights.txt");
+    vector<double> bias = read_double_data_from_file("../data-linear-model/bias.txt");
+    resize_double_vector(bias, n);
+    vector<double> x = read_double_data_from_file("../data-linear-model/xtest.txt");
+    vector<double> y_ground_truth = read_double_data_from_file("../data-linear-model/ytest.txt");
+    resize_double_vector(y_ground_truth, n);
+    vector<double> y_expected_score = read_double_data_from_file("../data-linear-model/yclassificationscore.txt");
+    resize_double_vector(y_expected_score, n);
+    
+    print_double_vector_comma_separated(weights, "weights");
+    print_double_vector_comma_separated(bias, "bias");
+    print_double_vector_comma_separated(x, "x");
+    print_double_vector_comma_separated(y_ground_truth, "y_ground_truth");
+    print_double_vector_comma_separated(y_expected_score, "y_expected_score");
+
+    // Step 1: Setup CryptoContext
+
+    // A. Specify main parameters
+    /* A1) Multiplicative depth:
+   * The CKKS scheme we setup here will work for any computation
+   * that has a multiplicative depth equal to 'multDepth'.
+   * This is the maximum possible depth of a given multiplication,
+   * but not the total number of multiplications supported by the
+   * scheme.
+   *
+   * For example, computation f(x, y) = x^2 + x*y + y^2 + x + y has
+   * a multiplicative depth of 1, but requires a total of 3 multiplications.
+   * On the other hand, computation g(x_i) = x1*x2*x3*x4 can be implemented
+   * either as a computation of multiplicative depth 3 as
+   * g(x_i) = ((x1*x2)*x3)*x4, or as a computation of multiplicative depth 2
+   * as g(x_i) = (x1*x2)*(x3*x4).
+   *
+   * For performance reasons, it's generally preferable to perform operations
+   * in the shorted multiplicative depth possible.
+   */
+    uint32_t multDepth = 2;
+
+    /* A2) Bit-length of scaling factor.
+   * CKKS works for real numbers, but these numbers are encoded as integers.
+   * For instance, real number m=0.01 is encoded as m'=round(m*D), where D is
+   * a scheme parameter called scaling factor. Suppose D=1000, then m' is 10 (an
+   * integer). Say the result of a computation based on m' is 130, then at
+   * decryption, the scaling factor is removed so the user is presented with
+   * the real number result of 0.13.
+   *
+   * Parameter 'scaleModSize' determines the bit-length of the scaling
+   * factor D, but not the scaling factor itself. The latter is implementation
+   * specific, and it may also vary between ciphertexts in certain versions of
+   * CKKS (e.g., in FLEXIBLEAUTO).
+   *
+   * Choosing 'scaleModSize' depends on the desired accuracy of the
+   * computation, as well as the remaining parameters like multDepth or security
+   * standard. This is because the remaining parameters determine how much noise
+   * will be incurred during the computation (remember CKKS is an approximate
+   * scheme that incurs small amounts of noise with every operation). The
+   * scaling factor should be large enough to both accommodate this noise and
+   * support results that match the desired accuracy.
+   */
+    uint32_t scaleModSize = 50;
+
+    /* A3) Number of plaintext slots used in the ciphertext.
+   * CKKS packs multiple plaintext values in each ciphertext.
+   * The maximum number of slots depends on a security parameter called ring
+   * dimension. In this instance, we don't specify the ring dimension directly,
+   * but let the library choose it for us, based on the security level we
+   * choose, the multiplicative depth we want to support, and the scaling factor
+   * size.
+   *
+   * Please use method GetRingDimension() to find out the exact ring dimension
+   * being used for these parameters. Give ring dimension N, the maximum batch
+   * size is N/2, because of the way CKKS works.
+   */
+    uint32_t batchSize = n;
+
+    /* A4) Desired security level based on FHE standards.
+   * This parameter can take four values. Three of the possible values
+   * correspond to 128-bit, 192-bit, and 256-bit security, and the fourth value
+   * corresponds to "NotSet", which means that the user is responsible for
+   * choosing security parameters. Naturally, "NotSet" should be used only in
+   * non-production environments, or by experts who understand the security
+   * implications of their choices.
+   *
+   * If a given security level is selected, the library will consult the current
+   * security parameter tables defined by the FHE standards consortium
+   * (https://homomorphicencryption.org/introduction/) to automatically
+   * select the security parameters. Please see "TABLES of RECOMMENDED
+   * PARAMETERS" in  the following reference for more details:
+   * http://homomorphicencryption.org/wp-content/uploads/2018/11/HomomorphicEncryptionStandardv1.1.pdf
+   */
+    CCParams<CryptoContextCKKSRNS> parameters;
+    parameters.SetMultiplicativeDepth(multDepth);
+    parameters.SetScalingModSize(scaleModSize);
+    parameters.SetBatchSize(batchSize);
+
+    CryptoContext<DCRTPoly> cc = GenCryptoContext(parameters);
+
+    // Enable the features that you wish to use
+    cc->Enable(PKE);
+    cc->Enable(KEYSWITCH);
+    cc->Enable(LEVELEDSHE);
+    cc->Enable(ADVANCEDSHE);
+    cout << "CKKS scheme is using ring dimension " << cc->GetRingDimension() << endl << endl;
+
+    // B. Step 2: Key Generation
+    /* B1) Generate encryption keys.
+   * These are used for encryption/decryption, as well as in generating
+   * different kinds of keys.
+   */
+    auto keys = cc->KeyGen();
+
+    /* B2) Generate the digit size
+   * In CKKS, whenever someone multiplies two ciphertexts encrypted with key s,
+   * we get a result with some components that are valid under key s, and
+   * with an additional component that's valid under key s^2.
+   *
+   * In most cases, we want to perform relinearization of the multiplicaiton
+   * result, i.e., we want to transform the s^2 component of the ciphertext so
+   * it becomes valid under original key s. To do so, we need to create what we
+   * call a relinearization key with the following line.
+   */
+    cc->EvalMultKeyGen(keys.secretKey);
+
+    /* B3) Generate the rotation keys
+   * CKKS supports rotating the contents of a packed ciphertext, but to do so,
+   * we need to create what we call a rotation key. This is done with the
+   * following call, which takes as input a vector with indices that correspond
+   * to the rotation offset we want to support. Negative indices correspond to
+   * right shift and positive to left shift. Look at the output of this demo for
+   * an illustration of this.
+   *
+   * Keep in mind that rotations work over the batch size or entire ring dimension (if the batch size is not specified).
+   * This means that, if ring dimension is 8 and batch
+   * size is not specified, then an input (1,2,3,4,0,0,0,0) rotated by 2 will become
+   * (3,4,0,0,0,0,1,2) and not (3,4,1,2,0,0,0,0).
+   * If ring dimension is 8 and batch
+   * size is set to 4, then the rotation of (1,2,3,4) by 2 will become (3,4,1,2).
+   * Also, as someone can observe
+   * in the output of this demo, since CKKS is approximate, zeros are not exact
+   * - they're just very small numbers.
+   */
+    // cc->EvalRotateKeyGen(keys.secretKey, {0});
+    cc->EvalSumKeyGen(keys.secretKey);
+
+    // Step 3: Encoding and encryption of inputs
+
+    // Encoding as plaintexts
+    Plaintext pt_x = cc->MakeCKKSPackedPlaintext(x);
+    Plaintext pt_weights = cc->MakeCKKSPackedPlaintext(weights);
+    Plaintext pt_bias = cc->MakeCKKSPackedPlaintext(bias);
+
+    cout << "Input pt_x: " << pt_x << endl;
+    cout << "Input pt_weights: " << pt_weights << endl;
+    cout << "Input pt_bias: " << pt_bias << endl;
+    
+
+    // Encrypt the encoded vectors
+    auto ct_x = cc->Encrypt(keys.publicKey, pt_x);
+    auto ct_weights = cc->Encrypt(keys.publicKey, pt_weights);
+    auto ct_bias = cc->Encrypt(keys.publicKey, pt_bias);
+
+    // Step 4: Evaluation
+
+    auto ct_res = cc->EvalInnerProduct(ct_x, ct_weights, n);
+    vector<double> mask = {1.0, 0.0, 0.0, 0.0};
+    Plaintext pt_mask = cc->MakeCKKSPackedPlaintext(mask);
+    ct_res = cc->EvalMult(ct_res, pt_mask);
+    ct_res += ct_bias;
+
+    // Step 5: Decryption and output
+    Plaintext result;
+    // We set the cout precision to 8 decimal digits for a nicer output.
+    // If you want to see the error/noise introduced by CKKS, bump it up
+    // to 15 and it should become visible.
+    cout.precision(8);
+
+    cout << endl << "Results of homomorphic computations: " << endl;
+
+    cc->Decrypt(keys.secretKey, ct_res, &result);
+    result->SetLength(batchSize);
+    cout << "computed classification score = " << result;
+    cout << "Estimated precision in bits: " << result->GetLogPrecision() << endl;
+    print_double_vector_comma_separated(y_expected_score, "y_expected_score");
+
+    cout << "SVM linear terminated gracefully ... !\n";
+
+    return 0;
+}
